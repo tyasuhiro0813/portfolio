7afa25b8fc2bf13e5e605411774343813b9f4c0e
tyasuhiro0813 <tyasuhiro0813@gmail.com>
2022-08-16 21:03:26 +0900

    ログインページ作成

---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="section container column is-half">
+        <b-field label="Email">
+            <b-input type="email"
+                v-model="email">
+            </b-input>
+        </b-field>
+
+        <b-field label="Password">
+            <b-input v-model="password" type="password" ></b-input>
+        </b-field>
+
+        <b-field class="is-flex is-justify-content-center"><!-- Label left empty for spacing -->
+            <p class="control">
+                <b-button label="Login" type="is-primary" @click="login"/>
+            </p>
+        </b-field>
+    </section>
+</template>
+
+<script>
+export default {
+    layout: "login-layout",
+    data(){
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            if(this.email === "") {
+                alert("メールアドレスを入力してください。")
+                return
+            }
+            if(this.password === "") {
+                alert("パスワードを入力してください。")
+                return
+            }
+            console.log(this.email, this.password)
+        }
+    } 
+}
+</script>
