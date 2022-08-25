191d24449773585033639a826c39e480e41d05cc
tyasuhiro0813 <tyasuhiro0813@gmail.com>
2022-08-25 23:41:01 +0900

    resister.vueアップデート

--- components/Mypost.vue
+++ components/Mypost.vue
@@ -2,30 +2,30 @@
     <section class="is-flex">
         <section class="control column is-half is-justify-content-center">
             <section>
-                店名：{{ name }}
+                店名： {{ name }}
             </section>
             <section>
-                エリア：{{ area }}
+                エリア： {{ area }}
             </section>
             <section>
-                ジャンル：{{ genre }}
+                ジャンル： {{ genre }}
             </section>
             <section>
-                評価：{{ rate }}
+                評価： {{ rate }}
             </section>
         </section>
         <section class="control column is-half is-justify-content-center">
             <section>
-                行った日：{{ date }}
+                行った日： {{ date }}
             </section>
             <section>
-                行った時間帯：{{ time }}
+                行った時間帯： {{ time }}
             </section>
             <section>
-                人数：{{ number }}
+                人数： {{ number }}
             </section>
             <section>
-                おすすめ：{{ recommend }}
+                おすすめ： {{ recommend }}
             </section>
         </section>
     </section>
@@ -33,6 +33,17 @@
 
 <script>
 export default {
-    
+    data(){
+        return {
+            name: "",
+            area: "東京",
+            genre: "居酒屋",
+            rate: "",
+            date: "2022/08/24",
+            time: "19:00",
+            member: "4",
+            recommend: ""
+        }
+    }
 }
 </script>
--- pages/register.vue
+++ pages/register.vue
@@ -6,37 +6,30 @@
                     <b-input v-model="name"></b-input>
                 </b-field>
 
-                <b-field label="エリア"
-                    horizontal>
+                <b-field label="エリア" horizontal>
                     <b-input v-model="area" maxlength="30">
                     </b-input>
                 </b-field>
 
-                <b-field label="ジャンル"
-                    horizontal>
+                <b-field label="ジャンル" horizontal>
                     <b-input v-model="genre" maxlength="30"></b-input>
                 </b-field>
 
                 <b-field label="評価" horizontal>
-                    <b-input v-model="rate">
-
-                    </b-input>
+                    <b-rate v-model="rate"></b-rate>
                 </b-field>
             </section>
 
             <section class="section container column is-half">
                 <b-field label="来店日" horizontal>
-                    <b-input v-model="date"></b-input>
+                    <b-datepicker v-model="date"></b-datepicker>
                 </b-field>
 
-                <b-field label="来店時間"
-                    horizontal>
-                    <b-input v-model="time" maxlength="30">
-                    </b-input>
+                <b-field label="来店時間" horizontal>
+                    <b-timepicker v-model="time"></b-timepicker>
                 </b-field>
 
-                <b-field label="来店人数"
-                    horizontal>
+                <b-field label="来店人数" horizontal>
                     <b-input v-model="member" maxlength="30"></b-input>
                 </b-field>
 
@@ -70,7 +63,45 @@ export default {
             date: "",
             time: "",
             member: "",
-            recommend: ""
+            recommend: "",
+            share: false
+        }
+    },
+    methods: {
+        register(){
+            if(this.name === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.area === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.genre === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.rate === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.date === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.time === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.member === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            if(this.recommend === "") {
+                alert("未入力項目があります。")
+                return
+            }
+            console.log(this.name, this.rate, this.date, this.time, this.share)
         }
     }
 }
